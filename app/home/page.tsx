c2547cb6bd4056edbb89e2bf576574d8cc6aef07
Hakeem <mwo248@yahoo.com>
2023-08-01 07:41:27 +0100

    title

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,17 +4,24 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 // TODO: HTML structure for home page
+// 1. fix web3made logo, zoomed out
 export default function page() {
   return (
     <div className={styles.container}>
-      <nav className="flex items-center">
-        <Image src="/vercel.svg" alt="epic" width="50" height="50" />
+      <nav className="flex justify-between items-start p-4">
+        <Image src="/web3made.png" alt="epic" width="50" height="50" />
 
-        <ConnectButton />
+        <ConnectButton label="login" />
       </nav>
       <main>
-        <div>
-          <h1>Reservoir NFT Explorer</h1>
+        <div className="flex flex-col gap-3">
+          <span className="text-3xl">Explore Real Estate</span>
+          <span className="flex items-center gap-3 text-3xl">
+            on
+            <Image src="/ethereum-logo.png" alt="eth" width="40" height="40" />
+            Ethereum
+          </span>
+
           <h2>Explore NFTs from the Reservoir Protocol</h2>
         </div>
       </main>
