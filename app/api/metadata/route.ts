5c173bca039afc9d39847be5b6e47bad20002c0c
Hakeem <mwo248@yahoo.com>
2023-08-07 08:54:37 +0100

    ratchet form created

---
+++ app/api/metadata/route.ts
@@ -0,0 +1,8 @@
+// 1. upload metadata to bundlr network
+// 2. return metadata url
+// 3. pass metadata url to mintNFT function
+
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+  } catch (error) {}
+};
---
+++ app/home/create/page.tsx
@@ -0,0 +1,5 @@
+import Create from "./create";
+
+export default function page() {
+  return <Create />;
+}
