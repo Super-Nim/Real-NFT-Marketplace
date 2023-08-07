cc0366685eb95d17138ff6088bea8ed846123fb8
Hakeem <mwo248@yahoo.com>
2023-08-07 09:04:15 +0100

    bundlr config

--- app/api/metadata/route.ts
+++ app/api/metadata/route.ts
@@ -2,7 +2,7 @@
 // 2. return metadata url
 // 3. pass metadata url to mintNFT function
 
-export const POST = async (req: Request, res: NextResponse) => {
+export const POST = async (req: Request) => {
   try {
   } catch (error) {}
 };
